8ef734240b21a50aa41898cca8b2f777cc0f87c1
IbrahimElrifi <ibrahimelrifi95@gmail.com>
2023-11-25 17:48:19 -0800

    rsa

---
+++ factors.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int num = 239809320265259;
+    long int factor1 = 2;
+    long int factor2;
+
+    while (num % factor1)
+    {
+        if (factor1 <= num)
+        {
+
